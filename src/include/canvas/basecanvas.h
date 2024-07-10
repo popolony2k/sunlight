8cfca82c11a7a3b4958e52780e63120fedde0928
PopolonY2k <popolony2k@popolony2k.com.br>
2024-07-09 22:21:47 -0300

    Added license to library header and gitignore adjustments

--- src/include/canvas/basecanvas.h
+++ src/include/canvas/basecanvas.h
@@ -1,8 +1,21 @@
 /*
- * basecanvas.h
+ * Copyright (c) since 2021 by PopolonY2k and Leidson Campos A. Ferreira
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
  *
- *  Created on: Jul 24, 2021
- *      Author: popolony2k
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ * claim that you wrote the original software. If you use this software
+ * in a product, an acknowledgment in the product documentation would be
+ * appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ * misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
  */
 
 #ifndef __BASECANVAS_H__
