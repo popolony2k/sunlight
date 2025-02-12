e1f13fb4939e6b6678f76130bd732e67a7e36a98
Lara <nikkochocho@gmail.com>
2025-02-12 19:31:24 -0300

    Added sample and map

---
+++ samples/resources/map/Tileset_Clouds_variant.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Tileset_Clouds_variant" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="images/Clouds_variant.png" width="96" height="96"/>
+</tileset>
--- samples/tilemaprenderer/tilemaprenderer_test.cpp
+++ samples/tilemaprenderer/tilemaprenderer_test.cpp
@@ -21,6 +21,37 @@
 #include "tilemaprenderer_test.h"
 #include "renderer/tilemaprenderer.h"
 
+// FIXME: temporario
+SunLight :: Renderer :: TileMapRenderer  *pRenderer = NULL;
+
+void MoveCameraUp( SunLight :: Input :: ControllerType type, int nId )  {
+    pRenderer -> MoveCameraUp();
+}
+
+void MoveCameraDown( SunLight :: Input :: ControllerType type, int nId )  {
+    pRenderer -> MoveCameraDown();
+}
+
+void MoveCameraLeft( SunLight :: Input :: ControllerType type, int nId )  {
+    pRenderer -> MoveCameraLeft();
+}
+
+void MoveCameraRight( SunLight :: Input :: ControllerType type, int nId )  {
+    pRenderer -> MoveCameraRight();
+}
+
+void ZoomIn( SunLight :: Input :: ControllerType type, int nId )  {
+    pRenderer -> ZoomIn();
+}
+
+void ZoomOut( SunLight :: Input :: ControllerType type, int nId )  {
+    pRenderer -> ZoomOut();
+}
+
+void ResetZoom( SunLight :: Input :: ControllerType type, int nId )  {
+    pRenderer -> ResetZoom();
+}
+
 int main( int argc, char **argv ) {
     SunLight :: TileMap :: stDimension2D     viewport;
     SunLight :: Renderer :: TileMapRenderer  renderer( DISPLAY_W,
@@ -29,18 +60,41 @@ int main( int argc, char **argv ) {
                                                        FRAMES_PER_SECOND,
                                                        false );
 
+    pRenderer = &renderer;
     renderer.SetScrollStepSize( W_SCROLL_STEP_SIZE, H_SCROLL_STEP_SIZE );
     renderer.SetViewControlMode( SunLight :: Renderer :: ViewControlMode :: VIEW_CONTROL_MODE_ACTIVE );
+
+    // Setup keyboard controller
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_S, std :: bind( &MoveCameraUp, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_W, std :: bind( &MoveCameraDown, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_D, std :: bind( &MoveCameraLeft, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_A, std :: bind( &MoveCameraRight, std :: placeholders::_1, std :: placeholders :: _2 ) );
+
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_DOWN, std :: bind( &MoveCameraUp, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_UP, std :: bind( &MoveCameraDown, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_RIGHT, std :: bind( &MoveCameraLeft, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_LEFT, std :: bind( &MoveCameraRight, std :: placeholders::_1, std :: placeholders :: _2 ) );
+
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_PAGE_UP, std :: bind( &ZoomOut, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_PAGE_DOWN, std :: bind( &ZoomIn, std :: placeholders::_1, std :: placeholders :: _2 ) );
+    renderer.SetUserKeyEventHandler( SunLight :: Input :: KEY_HOME, std :: bind( &ResetZoom, std :: placeholders::_1, std :: placeholders :: _2 ) );
 
     viewport.pos.x = VIEWPORT_POS_X;
     viewport.pos.y = VIEWPORT_POS_Y;
     viewport.size.nWidth  = VIEWPORT_WIDTH;
     viewport.size.nHeight = VIEWPORT_HEIGHT;
 
+    renderer.GetViewport().SetPreferredZoom( DEFAULT_ZOOM_SCALE_POS );
     renderer.GetViewport().SetZoom( DEFAULT_ZOOM_SCALE_POS );
     renderer.GetViewport().SetDimension2D( viewport );
-    renderer.SetDrawFPS( true );
+    renderer.SetDrawFPS( ENABLE_FPS_SHOW_LABEL );
     renderer.Start();
+
+    if( !renderer.LoadMap( "/home/lara/Projects/C_CPP/sunlight/samples/resources/map/Cloud_Map.tmx", DEFAULT_MAP_ALIGNMENT ) )  {
+        printf("Error loading map\n" );
+        return EXIT_FAILURE;
+    } 
+
     renderer.Run();
     renderer.Stop();
 
