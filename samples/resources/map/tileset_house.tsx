3ed7b965143fad2592cff3309db8d3c30c841160
Lara <nikkochocho@gmail.com>
2025-02-12 23:31:22 -0300

    fixed map

--- samples/resources/map/tileset_house.tsx
+++ samples/resources/map/tileset_house.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Tileset_House" tilewidth="32" tileheight="32" tilecount="10" columns="5">
- <image source="images/House.png" width="160" height="64"/>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset_house" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="images/house.png" width="160" height="64"/>
  <tile id="3">
   <animation>
    <frame tileid="3" duration="200"/>
