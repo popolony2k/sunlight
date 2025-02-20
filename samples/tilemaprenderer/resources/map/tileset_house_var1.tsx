7f3be9b2c0d5aaddf19bc9ae8d28d5615b0a561f
PopolonY2k <popolony2k@popolony2k.com.br>
2025-02-20 19:21:20 -0300

    Fixed directories removed due gitignore conflicts (#18)

---
+++ samples/tilemaprenderer/resources/map/tileset_house_var1.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tileset_house_var1" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="images/house_var1.png" width="160" height="64"/>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="8" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
